18453fc39c12df9b2cf2e562e311ac23bbce23b7
Mari-8 <ryanaltimari8@gmail.com>
2021-03-12 17:55:07 -0600

    commit tree 3858049d4902eba4738a4d943c7d4391017ae384

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 2 
+let num2 = 31 
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 10) + 1 
+
+let num3 = 24
+let num4 = 5 
+const mod = num3 % num4
+
+const numbers = (1, 2, 6, 8, 13, 18, 20)
+const max = Math.max(numbers)
